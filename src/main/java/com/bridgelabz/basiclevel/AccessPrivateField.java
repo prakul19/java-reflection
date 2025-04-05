8462b8593f2e8f8653476c6570377d2eb815643a
prakul19 <prakulagarwal5@gmail.com>
2025-04-05 15:23:47 +0530

    [prakul] stats: program to access class Person with a private field age using Reflection

---
+++ src/main/java/com/bridgelabz/basiclevel/AccessPrivateField.java
@@ -0,0 +1,25 @@
+package com.bridgelabz.basiclevel;
+import java.lang.reflect.Field;
+class Person {
+    private int age = 25;
+    public void displayAge() {
+        System.out.println("Age: " + age);
+    }
+}
+public class AccessPrivateField {
+    public static void main(String[] args) {
+        try {
+            Person person = new Person();
+            Class<?> cls = person.getClass();
+            // Get the age which is private
+            Field ageField = cls.getDeclaredField("age");
+            ageField.setAccessible(true);
+            ageField.set(person, 40);
+            int updatedAge = (int) ageField.get(person);
+            System.out.println("Updated Age using Reflection: " + updatedAge);
+            person.displayAge();
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
+    }
+}
